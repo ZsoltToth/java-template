991edd224c4acff17c87ecfac2dfe69c00a81b6c
Zsolt Toth <tothzs87@gmail.com>
2021-02-16 17:28:07 +0100

    style: UnusedImports are checked by checkstyle

--- src/main/java/hu/iit/uni/miskolc/advanced/java/App.java
+++ src/main/java/hu/iit/uni/miskolc/advanced/java/App.java
@@ -1,7 +1,10 @@
 package hu.iit.uni.miskolc.advanced.java;
 
+import java.util.Collection;
+
 public class App {
     public static void main(String[] args) {
+        String name = "Java";
         System.out.println("Hello World!");
     }
 }
