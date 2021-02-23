5206681ff3a53f4bf2084f53a1d9f553769e9c84
Zsolt Toth <tothzs87@gmail.com>
2021-02-23 09:42:02 +0100

    test: AppTest was added to improve the coverage and pass the quality gate

---
+++ src/test/java/hu/iit/uni/miskolc/advanced/java/AppTest.java
@@ -0,0 +1,15 @@
+package hu.iit.uni.miskolc.advanced.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void test(){
+        App app = new App();
+        App.main(new String[] {});
+    }
+
+}
